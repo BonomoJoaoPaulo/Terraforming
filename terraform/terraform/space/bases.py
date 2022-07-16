2a03d88f61aba2c54a94eb517945b36be4a2a36e
joaovictorvolpato <joaovictorvolpato@github.com>
2022-07-16 18:15:56 -0300

    new base logic

--- terraform/terraform/space/bases.py
+++ terraform/terraform/space/bases.py
@@ -19,107 +19,79 @@ class SpaceBase(Thread):
     def print_space_base_info(self):
         print(f"🔭 - [{self.name}] → 🪨  {self.uranium}/{self.constraints[0]} URANIUM  ⛽ {self.fuel}/{self.constraints[1]}  🚀 {self.rockets}/{self.constraints[2]}")
     
-    def consume_resources_to_create_rocket(self, rocket_name, lock2, enough_resources_to_create_any_rocket):
-        with lock2:
-            print("chegou no wait")
-            enough_resources_to_create_any_rocket.wait()
-        print('chamou o base rocket')
+    def consume_resources_to_create_rocket(self, rocket_name):
         self.base_rocket_resources(rocket_name)
 
-    def base_rocket_resources(self, rocket_name, lock1, moon_need_resources):
+    def base_rocket_resources(self, rocket_name):
         match rocket_name:
             case 'DRAGON':
-                if self.uranium > 35:
-                    self.uranium = self.uranium - 35
-                    if self.name == 'ALCANTARA' and self.fuel >= 70:
-                        globals.rocket_alc_sem_full.acquire()
+                self.uranium = self.uranium - 35
+                if self.name == 'ALCANTARA':
+                    if self.constraints[2] < self.rockets:
                         self.fuel = self.fuel - 70
                         self.rockets += 1
                         self.storage_rockets.append(Rocket('DRAGON'))
-                        globals.rocket_alc_sem_empty.release()
-                    elif self.name == 'MOON':
-                        if self.fuel >= 50:
-                            globals.rocket_moon_sem_full.acquire()
-                            self.fuel = self.fuel - 50
-                            self.rockets += 1
-                            self.storage_rockets.append(Rocket('DRAGON'))
-                            globals.rocket_alc_sem_empty.release()
-                        else:
-                            with lock1:
-                                moon_need_resources.notify()
-                    elif self.fuel >= 100:
-                        globals.rocket_capemoscow_sem_full.acquire()
-                        self.fuel = self.fuel - 100
+                elif self.name == 'MOON':
+                    if self.constraints[2] < self.rockets:
+                        self.fuel = self.fuel - 50
                         self.rockets += 1
                         self.storage_rockets.append(Rocket('DRAGON'))
-                        globals.rocket_capemoscow_sem_empty.release()
-                        
-                        
+                else:
+                    if self.constraints[2] < self.rockets:
+                        self.fuel = self.fuel - 100
+                        self.rockets += 1
+                        self.storage_rockets.append(Rocket('DRAGON'))    
             
             case 'FALCON':
-                if self.uranium > 35:
-                    self.uranium = self.uranium - 35
-                    if self.name == 'ALCANTARA' and self.fuel >= 100:
-                        globals.rocket_alc_sem_full.acquire()
+                self.uranium = self.uranium - 35
+                if self.name == 'ALCANTARA':
+                    if self.constraints[2] < self.rockets:
                         self.fuel = self.fuel - 100
                         self.rockets += 1
                         self.storage_rockets.append(Rocket('FALCON'))
-                        globals.rocket_alc_sem_empty.release()
-                    elif self.name == 'MOON':
-                        if self.fuel >= 90:
-                            globals.rocket_moon_sem_full.acquire()
-                            self.fuel = self.fuel - 90
-                            self.rockets += 1
-                            self.storage_rockets.append(Rocket('FALCON'))
-                            globals.rocket_moon_sem_empty.release() 
-                        else:
-                            with lock1:
-                                moon_need_resources.notify()
-                    elif self.fuel >= 120:
-                        globals.rocket_capemoscow_sem_full.acquire()
+                elif self.name == 'MOON':
+                    if self.constraints[2] < self.rockets:
+                        self.fuel = self.fuel - 90
+                        self.rockets += 1
+                        self.storage_rockets.append(Rocket('FALCON'))
+
+                else:
+                    if self.constraints[2] < self.rockets:
                         self.fuel = self.fuel - 120
                         self.rockets += 1
                         self.storage_rockets.append(Rocket('FALCON'))
-                        globals.rocket_capemoscow_sem_empty.release()
 
             case 'LION':
-                if self.uranium > 35:
-                    self.uranium = self.uranium - 35
-                    if self.name == 'ALCANTARA'  and self.fuel > 100:
-                        globals.rocket_alc_sem_full.acquire()
-                        self.fuel = self.fuel - 100
-                        self.rockets += 1
-                        self.storage_rockets.append(Rocket('LION'))
-                        globals.rocket_alc_sem_empty.release()
-                    elif self.fuel > 115:
-                        globals.rocket_capemoscow_sem_full.acquire()
+                self.uranium = self.uranium - 35
+                if self.constraints[2] < self.rockets:
+                    self.fuel = self.fuel - 100
+                    self.rockets += 1
+                    self.storage_rockets.append(Rocket('LION'))
+
+                else:
+                    if self.constraints[2] < self.rockets:
                         self.fuel = self.fuel - 115
                         self.rockets += 1
                         self.storage_rockets.append(Rocket('LION'))
-                        globals.rocket_capemoscow_sem_empty.release()
             case _:
                 print("Invalid rocket name")
 
     def base_launch_rocket(self):
         if self.name == 'ALCANTARA':
-            #decrementar o foguete de alcantara e lancar o puta
-            print(f'lancou o foguete da base {self.name}')
-            globals.rocket_alc_sem_empty.acquire()
-            rocket = self.storage_rockets.pop(0)
-            thread = Thread(target=self.voyageController, args=(rocket,))
-            thread.start()
+            if len(self.storage_rockets) > 0:
+                rocket = self.storage_rockets.pop(0)
+                thread = Thread(target=self.voyageController, args=(rocket,))
+                thread.start()
         elif self.name == 'MOON':
-            #decrementar o foguete de moon e lancar o puta
-            globals.rocket_moon_sem_empty.acquire()
-            rocket = self.storage_rockets.pop(0)
-            thread = Thread(target=self.voyageController, args=(rocket,))
-            thread.start()
+            if len(self.storage_rockets) > 0:
+                rocket = self.storage_rockets.pop(0)
+                thread = Thread(target=self.voyageController, args=(rocket,))
+                thread.start()
         else:
-            #decrementar o foguete de moscow
-            globals.rocket_capemoscow_sem_empty.acquire()
-            rocket = self.storage_rockets.pop(0)
-            thread = Thread(target=self.voyageController, args=(rocket,))
-            thread.start()
+            if len(self.storage_rockets) > 0:
+                rocket = self.storage_rockets.pop(0)
+                thread = Thread(target=self.voyageController, args=(rocket,))
+                thread.start()
 
         rocket = self.storage_rockets.pop(0)
         thread = Thread(target=self.voyageController, args=(rocket,))
@@ -129,21 +101,14 @@ class SpaceBase(Thread):
     def voyageController(self, rocket):
         print("chamou voyage controler")
         if self.name == 'ALCANTARA':
-            #decrementar o foguete de alcantara e lancar o puta
             rocket.voyage
-            print(f'lancou o {rocket.name} da base {self.name}')
             self.rockets -= 1
-            globals.rocket_alc_sem_full.release()
         elif self.name == 'MOON':
-            #decrementar o foguete de moon e lancar o puta
             rocket.voyage
             self.rockets -= 1
-            globals.rocket_moon_sem_full.release()
         else:
-            #decrementar o foguete de moscow
             rocket.voyage
             self.rockets -= 1
-            globals.rocket_capemoscow_sem_full.release()
 
     def refuel_oil(self, mines_resources):
         oil = mines_resources['oil_earth']
@@ -216,6 +181,23 @@ class SpaceBase(Thread):
             else:
                 return False
 
+    def Has_resources_to_create_lion(self):
+        if self.uranium > 75:
+            if self.name == "ALCANATARA":
+                #condition to create LION
+                if self.fuel > 220:
+                    return True
+                else:
+                    return False
+            else:
+                #condition to create LION
+                if self.fuel > 235:
+                    return True
+                else:
+                    return False
+        else:
+            return False
+
     def run(self):
         globals.acquire_print()
         self.print_space_base_info()
@@ -235,14 +217,30 @@ class SpaceBase(Thread):
                 hasResources = False
                 # Consome as minas de recursos ate ter recursos suficientes
 
-                while not self.verify_resources():
-                    if self.fuel < self.constraints[1]:
-                        print(f"pediu refuel f{self.name}")
-                        self.refuel_oil(mines_resources)
-                    if self.uranium < self.constraints[0]:
-                        print(f"pediu refuel u{self.name}")
-                        self.refuel_uranium(mines_resources)
-            
+                acquired = globals.handle_lion_mutex.acquire(blocking=False)
+
+                if not acquired:
+
+                    while not self.verify_resources():
+                        if self.fuel < self.constraints[1]:
+                            print(f"pediu refuel f{self.name}")
+                            self.refuel_oil(mines_resources)
+                        if self.uranium < self.constraints[0]:
+                            print(f"pediu refuel u{self.name}")
+                            self.refuel_uranium(mines_resources)
+                else:
+                    
+                    while not self.Has_resources_to_create_lion():
+                        if self.fuel < self.constraints[1]:
+                            print(f"pediu refuel f{self.name}")
+                            self.refuel_oil(mines_resources)
+                        if self.uranium < self.constraints[0]:
+                            print(f"pediu refuel u{self.name}")
+                            self.refuel_uranium(mines_resources)
+                            
+                    self.consume_resources_to_create_rocket('LION')
+                    self.base_launch_rocket()
+                    continue
             
             else:
                 hasResources = False
