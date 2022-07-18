d5b00cf2eaea6040f3b7b08e708c38b14bf1c9ed
BonomoJoaoPaulo <jpabonomo@gmail.com>
2022-07-18 20:07:05 -0300

    Refactored and commented planet.py

--- terraform/terraform/stars/planet.py
+++ terraform/terraform/stars/planet.py
@@ -13,18 +13,22 @@ class Planet(Thread):
 
     def nuke_detected(self, damage):
         if not self.is_planet_habitable():
-            # Proteger com Lock para caso seja atacado nos dois polos
+            # Protegemos com Lock para caso seja atacado nos dois polos (evitando condicao de corrida).
             globals.get_terraform_lock(self.name).acquire()
+            # Verifica se o dano causado deixara o valor negativo (impedindo que isso aconteca).
             if self.terraform - damage > 0:
                 self.terraform -= damage
             else:
                 self.terraform = 0
             print(f"⚠ - [NUKE DETECTION] - The planet {self.name} was bombed. {self.terraform}% UNHABITABLE")
+            # Libera o Lock.
             globals.get_terraform_lock(self.name).release()
-
+        # Verifica se o planeta esta habitavel ou nao. Caso esteja,
+        # retira-o da lista de planetas inabitaveis.
         if self.is_planet_habitable():
             globals.remove_planet_from_list_planets_unhabitable(self.name)
 
+    # Criamos essa funcao apenas para verificar se o planeta esta habitavel ou nao.
     def is_planet_habitable(self):
         return self.terraform <= 0
 
@@ -32,7 +36,9 @@ class Planet(Thread):
         print(f"🪐 - [{self.name}] → {self.terraform}% UNINHABITABLE")
 
     def run(self):
+        # Cria os locks proprios daquele planeta.
         globals.create_planet_and_poles_locks(self.name)
+        # Adiciona o planeta na lista de planetas inabitaveis.
         globals.append_in_unhabitale_planets(self.name)
         globals.acquire_print()
         self.print_planet_info()
